52374904e27cda4c734c62c598aac1937f2ca5ce
akhmelev <akhmelev@gmail.com>
2026-03-15 04:03:08 +0300

    add 1.11

---
+++ src/com/javarush/lesson11/Button.java
@@ -0,0 +1,17 @@
+package com.javarush.lesson11;
+
+public class Button extends Component {
+
+    private final String text;
+
+    public Button(int x, int y, String color, String text) {
+        super(x, y, color);
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Button[x=%d,y=%d,color=%s, text=%s",
+                super.getX(),getY(),getColor(),text);
+    }
+}
