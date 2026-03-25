2d85ce94d7ac4c57405f8fa148f091e5668badeb
akhmelev <akhmelev@gmail.com>
2026-03-25 13:05:38 +0300

    add 1.13

---
+++ src/com/javarush/lesson13/Department.java
@@ -0,0 +1,54 @@
+package com.javarush.lesson13;
+
+public class Department {
+
+    class InnerClass {
+
+    }
+
+    static void print() {
+        System.out.println("static method");
+    }
+
+    private static int counter;
+
+    static {
+        System.out.println("static block (1) counter=" + counter);
+    }
+
+    {
+        System.out.println("Logic (1) Field id=" + this.id);
+    }
+
+    private int id;
+
+    {
+        this.id = 8;
+        System.out.println("Logic (2) Field id=" + this.id);
+    }
+
+    static {
+        counter = 42;
+        System.out.println("static block (2) counter=" + counter);
+    }
+
+    public Department(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+               "id=" + id +
+               '}';
+    }
+
+    public static void main(String[] args) {
+        Department department1 = new Department(7);
+        System.out.println(department1);
+        Department department2 = new Department(77);
+        System.out.println(department2);
+        InnerClass innerClass = department1.new InnerClass();
+    }
+
+}
---
+++ src/com/javarush/lesson13/Runner.java
@@ -0,0 +1,26 @@
+package com.javarush.lesson13;
+
+ class RunLogger {
+    public static void main(String[] args) {
+        Application[] applications = {
+                new Application(),
+                new Application(),
+                new Application(),
+                new Application(),
+                new Application(),
+        };
+        applications[applications.length - 1].setIndex(42);
+        for (Application application : applications) {
+            Application.updateValueCounter(442);
+            String title = application.getTitle();
+            System.out.println(title);
+            System.LoggerFinder loggerFinder = System.LoggerFinder.getLoggerFinder();
+        }
+    }
+}
+
+ public class Runner{
+    public void print(String message){
+        System.out.println(message);
+    }
+}
