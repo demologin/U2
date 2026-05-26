78e9b2209a1d4ef4dfa745e1c568ec30e9ee1792
CodeMashine <petemashine@gmail.com>
2026-05-26 19:33:11 +0300

    div : overrite some scalar methods

---
+++ src/com/javarush/module2/lesson02/calc/TypeSwitcher.java
@@ -0,0 +1,4 @@
+package com.javarush.module2.lesson02.calc;
+
+public class TypeSwitcher {
+}
---
+++ src/com/javarush/module2/lesson02/calc/VariableTypes.java
@@ -0,0 +1,7 @@
+package com.javarush.module2.lesson02.calc.api;
+
+public enum VariableTypes {
+	SCALAR ,
+	VECTOR,
+	MATRIX,
+}
