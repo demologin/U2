b9d11d364391d8e687808ec5c44eb00810bc144e
akhmelev <akhmelev@gmail.com>
2026-06-06 21:08:35 +0300

    lesson7 switch fix calc

--- src/com/javarush/module2/lesson07/calc/api/Add.java
+++ src/com/javarush/module2/lesson07/calc/api/Add.java
@@ -1,9 +1,7 @@
 package com.javarush.module2.lesson07.calc.api;
 
-import com.javarush.module2.lesson07.calc.model.Scalar;
 import com.javarush.module2.lesson07.calc.model.Var;
 
 public interface Add {
     Var add(Var other);
-    Var add(Scalar scalar);
 }
